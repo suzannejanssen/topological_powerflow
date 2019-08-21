d0ebcf8f191e6fda888a941d4ce8a11a9406f077
Suzanne Janssen <suzannejanssen@wlan-145-94-205-199.wlan.tudelft.nl>
2019-08-21 16:46:50 +0200

    Attempt for working algorithm

--- matrices.py
+++ matrices.py
@@ -1,4 +1,43 @@
 # Building matrices for power graph
 # Topology used is from Topology.JPG
 
+import numpy as np
 
+A = np.matrix([[0, 1, 1, 0, 0], [1, 0, 1, 1, 0], [1, 1, 0, 0, 0], [0, 1, 0, 0, 1], [0, 0, 0, 1, 0]])
+
+# admittance of each lines (1/reactance), adjust if necessary
+# w1 = 1/2
+# w2 = 1/4
+# w3 = 1/10
+# w4 = 1/20
+# w5 = 1/0.5
+
+w1 = 1
+w2 = 1
+w3 = 1
+w4 = 1
+w5 = 1
+
+W = np.matrix([[0, w1, w2, 0, 0], [w1, 0, w3, w4, 0], [w2, w3, 0, 0, 0], [0, w4, 0, 0, w5], [0, 0, 0, w5, 0]])
+
+def diagonal(W):
+    """Make a diagonal matrix D from W. 
+    Each diagonal entry is formed by sum of components of W of the corresponding row. 
+    Returns diagonal matrix D"""
+
+    rowsum = W.sum(axis=1)    #Axis is 1 to sum over the rows
+    D = np.zeros((W.shape[0], W.shape[0]))
+
+    for i in range(W.shape[0]):
+        D[i][i] = rowsum[i]
+
+    return D
+
+D = diagonal(W)
+
+Q = np.subtract(D,W)
+#print(Q)
+#print(np.linalg.eigvalsh(Q))
+
+Test = np.matrix([[2,-1,-1],[-1,2,-1],[-1,-1,2]])
+print(np.linalg.eigvalsh(Test))
