744284434fae301eab7902ebed52816ad0ffb769
Suzanne Janssen <suzannejanssen@wlan-145-94-204-131.wlan.tudelft.nl>
2019-09-03 14:02:45 +0200

    defined graph1

---
+++ Network_info/graph.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def run(graph1=False):
+
+    if graph1 == True:
+        A,W,B = graph1()
+    # elif graph == graph2:
+    #     G = graph2()
+    # elif graph == graph3:
+    #     G = graph3()
+    # else:
+    #     G = graph4()
+    
+    return A,W,B
+
+def graph1():
+    A = np.matrix([[0, 1, 1, 0, 0], [1, 0, 1, 1, 0], [1, 1, 0, 0, 0], [0, 1, 0, 0, 1], [0, 0, 0, 1, 0]])
+    w1 = 1
+    w2 = 1
+    w3 = 1
+    w4 = 1
+    w5 = 1
+
+    W = np.matrix([[0, w1, w2, 0, 0], [w1, 0, w3, w4, 0], [w2, w3, 0, 0, 0], [0, w4, 0, 0, w5], [0, 0, 0, w5, 0]])
+    # NxL weighted incidence matrix
+    B = np.matrix([[w1, w2, 0, 0, 0], [-w1, 0, -w3, w4, 0], [0, -w2, w3, 0, 0], [0, 0, 0, -w4, w5], [0, 0, 0, 0, -w5]])
+
+    return A,W,B
---
+++ run.py
@@ -0,0 +1,14 @@
+from Network_info import graph
+
+
+
+def main():
+    A,W,B = graph.run(graph1=True)
+    print(A)
+
+    return
+
+
+
+if __name__ == '__main__':
+    main()
